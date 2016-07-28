7c3af333edda5de5633a96272b93cac7a7bf494c
Marjon Siero <marjonsiero@gmail.com>
2016-07-28 15:40:53 +0200

    Adding nav bar toggle

--- memorylane-users/app/models/user.rb
+++ memorylane-users/app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :memories
   delegate :trips, :concerts, :achievements, to: :memories
   validates :email, presence: true
-  has_attached_file :avatar, styles: { medium: "600x600>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "600x600>", small: "100x100>", thumb: "40x40>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
